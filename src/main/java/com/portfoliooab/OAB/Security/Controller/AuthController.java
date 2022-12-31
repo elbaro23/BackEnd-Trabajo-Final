91061c9d0a7b2a6c7cd3199ce8690e0f734c6eb1
Oscar Baroni <elbaro_oab@hotmail.com>
2022-12-31 14:28:08 -0300

    Se cambio la URL de Firebase

--- src/main/java/com/portfoliooab/OAB/Security/Controller/AuthController.java
+++ src/main/java/com/portfoliooab/OAB/Security/Controller/AuthController.java
@@ -32,7 +32,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/auth")
 //@CrossOrigin(origins = {"https://frontendoab.web.app","http://localhost:4200"})
-@CrossOrigin(origins = "https://frontendoab.web.app")
+@CrossOrigin(origins = "https://frontendargprooab.web.app")
 //@CrossOrigin(origins = "http://localhost:4200")
 public class AuthController {
     @Autowired
