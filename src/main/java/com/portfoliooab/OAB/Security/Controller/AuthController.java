895e60e9e4045065b961d7b05afdb883551e8eed
Oscar Baroni <elbaro_oab@hotmail.com>
2023-01-03 18:18:42 -0300

    Cambió la URL del Frontend de Firebase

--- src/main/java/com/portfoliooab/OAB/Security/Controller/AuthController.java
+++ src/main/java/com/portfoliooab/OAB/Security/Controller/AuthController.java
@@ -31,7 +31,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/auth")
-@CrossOrigin(origins = {"https://frontendargprooab.web.app","http://localhost:4200"})
+@CrossOrigin(origins = {"https://frontend-oab.web.app/","http://localhost:4200"})
 
 public class AuthController {
     @Autowired
